7a206cb6132244dfb7a14834de55af7879d522d0
wangzican <zcabanj@ucl.ac.uk>
2022-02-15 18:37:04 +0000

    added xuebao collision logic

--- SuperHot_try/Assets/Scripts/FirstAidLogic.cs
+++ SuperHot_try/Assets/Scripts/FirstAidLogic.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class FirstAidLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    public bool end = false;
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger("collected");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (end) 
+        {
+            Destroy(gameObject);
+        }
         transform.Rotate(new Vector3(0f, 300f, 0f) * Time.deltaTime, Space.World);
     }
 }
