a503c863a2079daffe3c3d70957f4e605d6a8351
wangzican <zcabanj@ucl.ac.uk>
2022-02-17 18:29:08 +0000

    added ff movement

--- SuperHot_try/Assets/Scripts/Force_field_logic.cs
+++ SuperHot_try/Assets/Scripts/Force_field_logic.cs
@@ -9,13 +9,21 @@ public class Force_field_logic : MonoBehaviour
     public float m_Thrust = 1f;
     private Vector3 changebig = new Vector3(0.05f, 0.05f, 0.05f);
     private Vector3 changesmall = new Vector3(0.005f, 0.005f, 0.005f);
-    private Vector3 change = new Vector3(0f, 0f, 0f);
+    private Vector3 change_in_size = new Vector3(0f, 0f, 0f);
+    private Vector3 change_in_position = new Vector3(0f, 0f, 0f);
     private float sleep_time = 0f;
     private bool exploded = false;
+    private int movement = 0;
+    private float distance;
+    private float togo;
+    private float rate = 0.01f;
     private void Start()
     {
         transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         animator = GetComponent<Animator>();
+        movement = Random.Range(0, 4);
+        distance = Random.Range(5,15);
+        togo = distance;
     }
     private void OnTriggerStay(Collider other)
     {
@@ -32,6 +40,12 @@ public class Force_field_logic : MonoBehaviour
     }
     private void Update()
     {
+        explode();
+        movetype();
+    }
+
+    private void explode()
+    {
         if (sleep_time > 0f)
         {
             if(sleep_time < 1.5f)
@@ -47,16 +61,42 @@ public class Force_field_logic : MonoBehaviour
             if(exploded)
             {
                 exploded = false;
-                sleep_time = Random.Range(1, 5);
+                sleep_time = Random.Range(1, 6);
             }
-            change = changebig;
+            change_in_size = changebig;
         }
         else if (transform.localScale.y > 2.9f)
         { 
-            change = -changesmall;
+            change_in_size = -changesmall;
             exploded = true;
         }
-        transform.localScale += change;
+        transform.localScale += change_in_size;
+    }
+    private void movetype()
+    {
+        if (movement == 1)
+        {
+            move(transform.up);
+        }
+        else if (movement == 2)
+        {
+            move(transform.right);
+        }
+        else if (movement == 3)
+        {
+            move(transform.forward);
+        }
+    }
+    private void move(Vector3 direction)
+    {
+        Debug.Log(gameObject.name + " type: " + movement + " d: " + direction + " dist: " + distance + " togo: " + togo);
+        if (togo <= 0 || togo >= distance)
+        {
+            rate = -rate;
+        }
+        change_in_position = direction * rate;
+        togo += Mathf.Sign(rate) * Vector3.Magnitude(change_in_position);
+        transform.position += change_in_position;
     }
 
 }
